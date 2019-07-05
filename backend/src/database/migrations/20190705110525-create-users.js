ccfec0c771eb79f19135eb82f996f2cf31e5bfa9
Efraim Andrade <efraim.dev@gmail.com>
2019-07-05 09:06:16 -0300

    feat: Meetapp (backend)
    finish the first challenge, create user, update user, jwt authentication, password encryption, schema validation.

--- backend/src/database/migrations/20190705110525-create-users.js
+++ backend/src/database/migrations/20190705110525-create-users.js
@@ -20,7 +20,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      role: {
+      provider: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -9,5 +12,9 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
