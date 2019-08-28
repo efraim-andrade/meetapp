fb3bedd0eb7e033a9843c293442a811326685f44
Efraim Andrade <efraim.dev@gmail.com>
2019-08-27 22:20:10 -0300

    feat: (FrontEnd)
    finish the front end application.

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -45,7 +45,7 @@ class UserController {
       password: Yup.string()
         .min(6)
         .when('oldPassword', (oldPassword, field) =>
-          oldPassword ? field.require() : field
+          oldPassword ? field.required() : field
         ),
       passwordConfirm: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
